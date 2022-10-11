849602fc401f6968bf16b31f77905a78ccb31695
JennieJo <114523288+JennieJo@users.noreply.github.com>
2022-10-11 09:22:29 +0200

    Karta

---
+++ src/characters.java
@@ -0,0 +1,87 @@
+
+//här listar jag alla karaktärerna/väggarna/osv + gamla kart-ideer
+public class characters {
+
+    String cat;
+    String rat;
+    String blcube;
+    String bcube;
+    String ccube;
+    String pcube;
+    String wcube;
+    String rcube;
+    String star;
+    String ccir;
+    String gcir;
+    String wcir;
+    String heart;
+
+// 1 X
+    //System.out.println(color.ANSI_BLUE + "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄" +
+    //                "\n█.° ｡˚.° ｡˚.° ｡˚.° ｡˚.° ｡˚.° ｡˚.█" +
+    //                "\n█.° ｡˚▄▄▄▄▄▄▄▄▄▄▄▄ ｡˚.° ｡˚.° ｡˚.█" +
+    //                "\n█.° ｡˚█°｡˚.° ｡˚.° ｡˚.° ｡˚.█° ｡˚.█" +
+    //                "\n█.°" + color.ANSI_YELLOW + "★" + color.ANSI_RESET + color.ANSI_BLUE + ".°█ ｡˚.° ｡˚." + color.ANSI_RESET +  color.ANSI_CYAN +" ᓚᘏᗢ " + color.ANSI_RESET + color.ANSI_BLUE + ".° ｡█° ｡˚.█" +
+    //                "\n█.° ｡˚█ " + color.ANSI_RESET + "ᘛ⁐̤ᕐᐷ " + color.ANSI_BLUE + ".° ｡˚.° ｡˚.°█｡˚.°.█" +
+    //                "\n█▄▄▄▄▄█° ｡˚.° ｡˚.° ｡˚.° ｡˚█▄▄▄▄▄█" +
+    //                "\n█° ｡˚" + color.ANSI_RESET + color.ANSI_RED + " 웃 " + color.ANSI_RESET + color.ANSI_BLUE + " ｡˚.° ｡˚.° ｡˚.° ｡˚.° ｡˚.█" +
+    //                "\n█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█" + color.ANSI_RESET);
+
+//2 OK
+    //                        new String[] { " ", " ", ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, " " },
+    //                        new String[] { ca.wcube, ca.wcube, ca.wcube, " ", " ", " ", ca.wcube, " " },
+    //                        new String[] { ca.wcube, ca.star, ca.ccube, ca.rcube, " ", " ", ca.wcube, " " },
+    //                        new String[] { ca.wcube, ca.wcube, ca.wcube, " ", ca.rcube, ca.star, ca.wcube, " " },
+    //                        new String[] { ca.wcube, ca.star, ca.wcube, ca.wcube, ca.rcube, " ", ca.wcube, " " },
+    //                        new String[] { ca.wcube, " ", ca.wcube, " ", ca.star, " ", ca.wcube, ca.wcube },
+    //                        new String[] { ca.wcube, ca.rcube, " ", ca.rcube, ca.rcube, ca.rcube, ca.star, ca.wcube },
+    //                        new String[] { ca.wcube, " ", " ", " ", ca.star, " ", " ", ca.wcube },
+    //                        new String[] { ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube }
+
+//3 X
+
+    //                        new String[] { " ", " ", "┎", "⎺", "⎺", "⎺", "┒", " " },
+    //                        new String[] { " ", "▁", "▍", " ", " ", " ", "▍", " " },
+    //                        new String[] { "▍", ca.star, ca.ccube, " ", " ", " ", "▍", " " },
+    //                        new String[] { "▍", "—", "┒", ca.rcube, " ", " ", "▍", " " },
+    //                        new String[] { "▍", ca.star, "▍", " ", ca.rcube, ca.star, "▍", " " },
+    //                        new String[] { "▍", " ", "▍", " ", ca.rcube, " ", "▍", " " },
+    //                        new String[] { "▍", " ", "▍", " ", ca.star, " ", "⎺", "▍" },
+    //                        new String[] { "▍", ca.rcube, " ", ca.rcube, ca.rcube, ca.rcube, ca.star, "▍" },
+    //                        new String[] { "▍", " ", " ", " ", ca.star, " ", " ", "▍" },
+    //                        new String[] { "▍", "▁", "▁", "▁", "▁", "▁", "▁", "▍" }
+
+//4 X
+    //                        new String[] { " ", " ", "█", "▀", "▀", "▀", "█", " " },
+    //                        new String[] { "█", "▀", "▀", " ", " ", " ", "█", " " },
+    //                        new String[] { "█", ca.star, " ", " ", " ", " ", "█", " " },
+    //                        new String[] { "█", "▀", "█", " ", " ", ca.star, "█", " " },
+    //                        new String[] { "█", ca.star, "█", "■", " ", " ", "█", " " },
+    //                        new String[] { "█", " ", "█", " ", " ", " ", "█", " " },
+    //                        new String[] { "█", " ", " ", " ", ca.star, " ", "▀", "█" },
+    //                        new String[] { "█", ca.rcube, " ", ca.rcube, ca.rcube, ca.rcube, ca.star, "█" },
+    //                        new String[] { "█", " ", " ", " ", ca.star, " ", " ", "█" },
+    //                        new String[] { "█", "▄", "▄", "▄", "▄", "▄", "▄", "█" }
+
+//5 X
+    //                        new String[] { " ", " ", " ", " ", ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ".", "." },
+    //                        new String[] { " ", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ca.cat, ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ca.wcube, " ", " " },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ca.wcube, ca.wcube, ca.wcube },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ca.wcube },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ca.wcube },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ca.wcube },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ca.wcube },
+    //                        new String[] { ca.wcube, ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ca.wcube },
+    //                        new String[] { ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, ca.wcube, }
+
+    ////"♥", "♥"︎,"♥", "♥", "♥", "♥", "♥", "♥", "♥"
+}
